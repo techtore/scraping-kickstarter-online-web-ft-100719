791ab350bab2f18a6b832c43083212ad33a6d524
Victoria Hale <vic.hale74@gmail.com>
2019-10-26 16:48:04 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,7 +1,11 @@
+require 'pry'
 require 'nokogiri'
 
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
  
   kickstarter = Nokogiri::HTML(html)
+  binding.pry
 end
+
+create_project_hash
