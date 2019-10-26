67c022d4779f748ae2746683665f8a41d129b89c
Victoria Hale <vic.hale74@gmail.com>
2019-10-26 16:54:39 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,5 +8,7 @@ def create_project_hash
   binding.pry
 end
 
-# projects: kickstarter.css("li.project.grid_4")
 create_project_hash
+
+# projects: kickstarter.css("li.project.grid_4")
+# title: project.css("h2.bbcard_name strong a").text
